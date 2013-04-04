ed5c78c7239650c27e223ccddda2eaefb57451e4
Jason McVetta <jason.mcvetta@gmail.com>
2013-04-03 20:00:38 -0700

    add gpl

--- btoken_test.go
+++ btoken_test.go
@@ -1,4 +1,7 @@
-// Copyright (C) 2013 Jason McVetta, all rights reserved.
+// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released
+// under the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for
+// details.  Resist intellectual serfdom - the ownership of ideas is akin to
+// slavery.
 
 package btoken
 
