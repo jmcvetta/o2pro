b17bb70762fae74061740837ff8613bd86b1d401
Jason McVetta <jason.mcvetta@gmail.com>
2013-04-10 14:27:47 -0700

    Add mongodb:// prefix to dial url

--- server_test.go
+++ server_test.go
@@ -29,7 +29,7 @@ func setup(t *testing.T) {
 		t.Log("Initializing testAuthServer\n")
 	*/
 	log.SetFlags(log.Ltime | log.Lshortfile)
-	session, err := mgo.Dial("127.0.0.1")
+	session, err := mgo.Dial("mongodb://127.0.0.1")
 	if err != nil {
 		t.Fatal(err)
 	}
