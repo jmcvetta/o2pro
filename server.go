e95328b3e2c68f5dab5c05624de655629a809fd4
Jason McVetta <jason.mcvetta@gmail.com>
2013-04-10 13:54:59 -0700

    Fix date in license statement

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
-// Copyright (c) 2012-2013 Jason McVetta.  This is Free Software, released
-// under the terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for
-// details.  Resist intellectual serfdom - the ownership of ideas is akin to
-// slavery.
+// Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+// terms of the GPL v3.  See http://www.gnu.org/copyleft/gpl.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
 
 // Package btoken implements Oauth2-style bearer tokens.
 // https://tools.ietf.org/html/draft-ietf-oauth-v2-bearer-16
