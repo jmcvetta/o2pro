92d2287152e75d738c088edd15019c159ad7a5dc
Jason McVetta <jason.mcvetta@gmail.com>
2013-04-11 20:04:10 -0700

    use a method to return a HandlerFunc since a method cannot itself be cast to HandlerFuc

--- api.go
+++ api.go
@@ -19,55 +19,57 @@ var (
 	authRegex = regexp.MustCompile(authReStr)
 )
 
-func (s *Server) HandleAuthRequest(w http.ResponseWriter, r *http.Request) {
-	//
-	// Authenticate
-	//
-	str := r.Header.Get("Authorization")
-	malformed := "Malformed Authorization header"
-	matches := authRegex.FindStringSubmatch(str)
-	if len(matches) != 2 {
-		http.Error(w, malformed, http.StatusBadRequest)
+func (s *Server) AuthReqHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		//
+		// Authenticate
+		//
+		str := r.Header.Get("Authorization")
+		malformed := "Malformed Authorization header"
+		matches := authRegex.FindStringSubmatch(str)
+		if len(matches) != 2 {
+			http.Error(w, malformed, http.StatusBadRequest)
+			return
+		}
+		encoded := matches[1]
+		b, err := base64.StdEncoding.DecodeString(encoded)
+		if err != nil {
+			http.Error(w, malformed, http.StatusBadRequest)
+			return
+		}
+		parts := strings.Split(string(b), ":")
+		if len(parts) != 2 {
+			http.Error(w, malformed, http.StatusBadRequest)
+			return
+		}
+		username := parts[0]
+		password := parts[1]
+		u := url.UserPassword(username, password)
+		//
+		// Parse authorization request
+		//
+		dec := json.NewDecoder(r.Body)
+		var areq AuthRequest
+		err = dec.Decode(&areq)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, malformed, http.StatusBadRequest)
+			return
+		}
+		a, err := s.Authorize(u, areq)
+		switch {
+		case err == ErrUnauthorized:
+			http.Error(w, malformed, http.StatusUnauthorized)
+			return
+		case err != nil:
+			log.Println(err)
+			http.Error(w, malformed, http.StatusBadRequest)
+		}
+		//
+		// Authorization Granted
+		//
+		enc := json.NewEncoder(w)
+		enc.Encode(&a)
 		return
 	}
-	encoded := matches[1]
-	b, err := base64.StdEncoding.DecodeString(encoded)
-	if err != nil {
-		http.Error(w, malformed, http.StatusBadRequest)
-		return
-	}
-	parts := strings.Split(string(b), ":")
-	if len(parts) != 2 {
-		http.Error(w, malformed, http.StatusBadRequest)
-		return
-	}
-	username := parts[0]
-	password := parts[1]
-	u := url.UserPassword(username, password)
-	//
-	// Parse authorization request
-	//
-	dec := json.NewDecoder(r.Body)
-	var areq AuthRequest
-	err = dec.Decode(&areq)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, malformed, http.StatusBadRequest)
-		return
-	}
-	a, err := s.Authorize(u, areq)
-	switch {
-	case err == ErrUnauthorized:
-		http.Error(w, malformed, http.StatusUnauthorized)
-		return
-	case err != nil:
-		log.Println(err)
-		http.Error(w, malformed, http.StatusBadRequest)
-	}
-	//
-	// Authorization Granted
-	//
-	enc := json.NewEncoder(w)
-	enc.Encode(&a)
-	return
 }
